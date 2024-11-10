6e5ec576d6d0602ce8d3e780d4b05949fdadd6d2
Дмитрий Волков <roskey@mail.ru>
2024-11-10 23:42:14 +0300

    Домашнее задание Модуль 4/2-1

--- Module_4_2/main.py
+++ Module_4_2/main.py
@@ -1,8 +1,8 @@
-from My_module import my_module_1, my_module_2
-from My_module_2 import my_module_2_file_1, my_module_2_file_2
-from My_module_2.folder_1 import folder_1_file_1, folder_1_file_2
-from My_module_2.folder_1.folder_2 import folder_2_file_1, folder_2_file_2
-from My_module_2.folder_1.folder_2.folder_3 import folder_3_file_1, folder_3_file_2
+from Module_4_2.My_module import my_module_1, my_module_2
+from Module_4_2.My_module_2 import my_module_2_file_1, my_module_2_file_2
+from Module_4_2.My_module_2.folder_1 import folder_1_file_2, folder_1_file_1
+from Module_4_2.My_module_2.folder_1.folder_2 import folder_2_file_2, folder_2_file_1
+from Module_4_2.My_module_2.folder_1.folder_2.folder_3 import folder_3_file_2, folder_3_file_1
 
 """Не знаю, правильно ли я понял задание"""
 
